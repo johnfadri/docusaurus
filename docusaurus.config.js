ba017ed11ef34c5f53cc9ab83692db8336b5350d
john fadri <john.fadri@gmail.com>
2022-07-14 19:26:53 +0800

    modified docusaurus not hidden file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  "title": "Test Website",
+  "tagline": "Jack is Cool",
+  "url": "https://jdrfdocusaurus.netlify.app",
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
